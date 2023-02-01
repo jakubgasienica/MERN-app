611eecba95b04f2c8ee4cd95d1957e927e5cd5b4
jakubgasienica <gasienica.kuba@gmail.com>
2023-02-01 17:42:28 +0100

    set mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("server...");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
